71b40db5d691e195af12d3e1e6bd11a79243f700
westhide <westhide.dev@gmail.com>
2025-05-23 15:10:52 +0800

    wip: nixos config

--- nixos/options.nix
+++ nixos/options.nix
@@ -5,7 +5,7 @@
   username = "westhide";
   usermail = "westhide.dev@gmail.com";
   hostname = "westhide-nixos";
-  hardware = "lenovo-thinkpad-x13";
+  hardware = "lenovo-legion-16irx8h";
   proxyurl = null;
   wkgitdir = "~/Store/CodeHub";
 }
