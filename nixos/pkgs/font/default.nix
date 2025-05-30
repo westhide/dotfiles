4f4eb5f95e5ad5bf5ca04980a502d125038001ac
westhide <westhide.dev@gmail.com>
2025-05-30 18:04:23 +0800

    wip: nixos config

--- nixos/pkgs/font/default.nix
+++ nixos/pkgs/font/default.nix
@@ -1,7 +1,14 @@
-{ ... }:
+{ lib, ... }:
 
 {
-  # ln -s ~/dotfiles/fonts ~/.local/share/fonts
+  home = {
+    activation = {
+      link-fonts = lib.hm.dag.entryAfter [ "installPackages" ] ''
+        run [ ! -d ~/.local/share/fonts ] && [ -d ~/dotfiles/fonts ] && ln -s ~/dotfiles/fonts ~/.local/share/fonts
+      '';
+    };
+  };
+
   fonts.fontconfig = {
     enable = true;
     defaultFonts = {
--- nixos/pkgs/nodejs/default.nix
+++ nixos/pkgs/nodejs/default.nix
@@ -1,20 +1,23 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 
 {
-  # npm set prefix ~/.npm-store
-  # corepack enable --install-directory ~/.corepack-store/bin
   home = {
     sessionPath = [
       "$HOME/.npm-store/bin"
       "$HOME/.corepack-store/bin"
     ];
+    activation = {
+      init-corepack-store = lib.hm.dag.entryAfter [ "installPackages" ] ''
+        run ${pkgs.nodejs}/bin/npm set prefix ~/.npm-store
+        run mkdir -p ~/.corepack-store/bin
+        run ${pkgs.nodejs}/bin/corepack enable --install-directory ~/.corepack-store/bin
+      '';
+    };
   };
 
   home.packages = with pkgs; [
     nodejs
     bun
     turbo
-    # pnpm
-    # yarn
   ];
 }
