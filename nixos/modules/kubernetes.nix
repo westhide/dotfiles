6a6c80edceff7e4323039797279d7197a0b3f516
Neo <unknown@unknown.com>
2025-09-03 15:04:41 +0800

    feat: helm

--- nixos/modules/kubernetes.nix
+++ nixos/modules/kubernetes.nix
@@ -1,19 +1,22 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
-  networking.firewall.allowedTCPPorts = [
-    6443 # API Server
-    2379
-    2380 # Etcd
-  ];
-  networking.firewall.allowedUDPPorts = [
-    # 8472 # Flannel VXLAN
+  environment.systemPackages = with pkgs; [
+    kubernetes-helm
   ];
   services.k3s = {
     enable = true;
     role = "server";
     extraFlags = toString [
       # "--debug"
+    ];
+  };
+  networking.firewall = {
+    allowedTCPPorts = [
+      6443 # API Server
+    ];
+    allowedUDPPorts = [
+      # 8472 # Flannel VXLAN
     ];
   };
 }
