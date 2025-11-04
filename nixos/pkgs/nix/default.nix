9d5e85884cc5bbfdc83274ad90e57bef2ee4af44
Neo <wangyongzhao@avatolabs.com>
2025-11-04 13:37:04 +0800

    feat: nix-direnv

--- nixos/pkgs/nix/default.nix
+++ nixos/pkgs/nix/default.nix
@@ -5,4 +5,13 @@
     nixfmt-rfc-style
     nixd
   ];
+
+  programs = {
+    bash.enable = true;
+    direnv = {
+      enable = true;
+      enableBashIntegration = true;
+      nix-direnv.enable = true;
+    };
+  };
 }
