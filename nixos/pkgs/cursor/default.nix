6563d374eb2db537328986503e5aa76e75ad4722
Neo <wangyongzhao@avatolabs.com>
2025-12-19 17:13:53 +0800

    feat: antigravity

--- nixos/pkgs/cursor/default.nix
+++ nixos/pkgs/cursor/default.nix
@@ -2,6 +2,7 @@
 
 {
   home.packages = with pkgs; [
+    antigravity
     (code-cursor.override {
       commandLineArgs = "--ozone-platform-hint=auto --ozone-platform=wayland --gtk-version=4 --enable-wayland-ime";
     })
